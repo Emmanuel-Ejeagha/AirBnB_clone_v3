f440007358af6af508e4b82e550e3516bcfea021
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-03-31 15:53:27 +0000

    Updated views/index.py file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
-"""Creating a Flask app"""
+"""This module implement arule that returns status of the application"""
 
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
+import models
+from models.amentiy import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 # Defines route /status
-
-
-@app_views.route('/status', methods=['GET'])
-def status():
-    """ status route"""
-    data = {
-        "status": "OK"
-    }
-    response = jsonify(data)
-    response.status_code = 200
-    return response
+@app_views.route('/status', methods=strict_slashes=False)
+def view_status():
+    """View function that returns a json message"""
+    return jsonsify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 """an endpoint that retrieves the number of each objects by type"""
-
-
-def stats():
-    dict = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Places"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    response = jsonify(dict)
-    response.status_code = 200
-    return response
+def view_stats():
+    """View function that retrives the number of each object by type"""
+    return jsonify({
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Places),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
+    })
