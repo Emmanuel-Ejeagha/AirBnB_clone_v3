fcc0bde830ff4a6fc9068d42a5d45a16a22158d3
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-03-31 17:06:02 +0300

    API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Creating a Flask app"""
 from flask import Blueprint
+from api.v1.views.index import *
 
 # Create a Blueprint instance
 app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
-
-from api.v1.views.index import *
