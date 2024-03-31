f1470a8bcca6cafe39e8aebd50d797fb38917c74
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-03-31 15:21:06 +0000

    Updated app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,23 +4,36 @@
 from os import getenv
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasger.utils import swang_from
+
 
 app = Flask(__name__)
 # Register app_views as a blueprint to the Flask Instance
 app.register_blueprint(app_views)
-
-# Declare a method to handle teardown of the app context
+cors = CORS(app, resourses={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    storage.close()
+    """Removes the current SQLAlchemy Session"""
+    return storage.close()
 
-# Run the Flask server
 
+@app.errorhandler(404)
+def error(e):
+    """Handles 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone RESTful API',
+    'uiversion': 3
+}
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(getenv("HBNB_API_PORT", 5000))
+    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
+    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
     app.run(host=host, port=port, threaded=True)
