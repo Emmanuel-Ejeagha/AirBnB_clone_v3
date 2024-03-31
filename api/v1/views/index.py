e558e68ba597d2ffc30f3418389447e9b0ca7051
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-03-31 17:03:42 +0300

    API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,17 +20,18 @@ def status():
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
-    """an endpoint that retrieves the number of each objects by type"""
+"""an endpoint that retrieves the number of each objects by type"""
 
-dict = {
-    "amenities": storage.count("Amenity"),
-    "cities": storage.count("City"),
-    "places": storage.count("Places"),
-    "reviews": storage.count("Review"),
-    "states": storage.count("State"),
-    "users": storage.count("User"),
-}
+
+def stats():
+    dict = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Places"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
     response = jsonify(dict)
     response.status_code = 200
     return response
